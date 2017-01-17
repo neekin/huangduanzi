9515d300fe5bc50d3dd201fb93d009a41675d718
NeeKin <njgzs360@gmail.com>
2017-01-18 07:58:58 +0800

    test

--- huangduanzi/admin.py
+++ huangduanzi/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-# from . import models
-# admini.site.register(xxx)
+# 在后台注册类
+# from models import Huangduanzi
+# admini.site.register(Huangduanzi)
 # Register your models here.
+'''
+在admin后台 显示丰富的内容 如其他的字段
+class HuangduanziAdmin(admin.ModelAdmin):
+    list_display=('id','title','date')
+    list_fiter =('date',)
+admini.site.register(HuangduanziAdmin)
+'''
--- huangduanzi/models.py
+++ huangduanzi/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+# 创建models  
 # class huangduanzi(models.Model):
 #     pass
 #     def __unicode__(self):
 #         return self.title
+
