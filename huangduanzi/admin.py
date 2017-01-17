798b1a04a1f92801202e8da992543be7cc457d49
NeeKin <njgzs360@gmail.com>
2017-01-18 07:14:16 +0800

    first commit

---
+++ huangduanzi/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+# from . import models
+# admini.site.register(xxx)
+# Register your models here.
---
+++ huangduanzi/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(res):
+   return render(res,'index.html')
+    
+# Create your views here.
